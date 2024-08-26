7fb54e560290a95c33d88cd6ac4ff598c771e9af
MuhammadRizki8 <muhammadrizki@upi.edu>
2024-08-26 16:53:38 +0700

    fix hero sec

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,25 @@
 // src/components/HeroSection.jsx
 // import React from 'react';
 import heroImage from '../assets/images/pagaruyung2.jpg';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 const HeroSection = () => {
   return (
     <div className="bg-cover bg-center h-screen w-full" style={{ backgroundImage: `url(${heroImage})` }}>
       <div className=" flex flex-col items-center justify-center h-full bg-black bg-opacity-50 px-4">
-        <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold font-poppins text-center mb-4">Jelajahi Budaya dan Alam Minangkabau bersama Malala</h1>
-        <p className="text-white text-lg md:text-xl lg:text-2xl font-light font-poppins text-center mb-8">Mulai petualanganmu di tanah penuh budaya dan keindahan alam yang memukau. Temukan destinasi terbaik di Minangkabau.</p>
-        <div className="w-full max-w-md flex items-center">
-          <input type="text" placeholder="Cari destinasi..." className="w-full p-3 rounded-l-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 font-poppins" />
-          <button className="bg-blue-500 text-white p-3 rounded-r-lg hover:bg-blue-600">
-            <MagnifyingGlassIcon className="h-5 w-5" />
-          </button>
+        <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-normal font-poppins text-center mb-4 lg:w-3/4">Jelajahi Budaya dan Alam Minangkabau bersama Malala</h1>
+        <p className="text-white w-3/4 text-lg md:text-xl lg:text-2xl font-light font-poppins text-center mb-8">Mulai petualanganmu di tanah penuh budaya dan keindahan alam yang memukau. Temukan destinasi terbaik di Minangkabau.</p>
+
+        <div className="w-full max-w-md md:max-w-lg flex items-center p-2 space-x-2 bg-white rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
+          <div className="flex bg-gray-100 p-2 w-full space-x-4 rounded-lg items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <input className="bg-gray-100 outline-none flex-1 min-w-0" type="text" placeholder="Cari destinasi.." />
+          </div>
+
+          <div className="bg-green-700 hover:bg-green-800 py-2 px-8 text-white font-semibold rounded-md hover:shadow-lg transition duration-300 cursor-pointer">
+            <span>Cari</span>
+          </div>
         </div>
       </div>
     </div>
--- src/components/LocationSection.jsx
+++ src/components/LocationSection.jsx
@@ -1,88 +1,40 @@
-// src/components/LocationSection.jsx
-import React, { useState } from 'react';
-
-// Gambar dummy untuk lokasi dan destinasi (gantikan dengan gambar yang sesuai)
-import solokImage from '../assets/images/solok.jpg';
-import padangImage from '../assets/images/padang.jpg';
-import bukittinggiImage from '../assets/images/bukittinggi.jpeg';
-
-import relatedImage from '../assets/images/solok.jpg'; // Dummy image for related destinations
+import React, { useState, useEffect } from 'react';
 import DestinationCard from './DestinationCard';
-const locations = [
-  {
-    name: 'Solok',
-    image: solokImage,
-    description: 'Solok dikenal dengan keindahan alamnya yang menakjubkan, seperti Danau Singkarak dan perkebunan teh yang luas.',
-    categories: ['Alam', 'Kuliner', 'Budaya'],
-  },
-  {
-    name: 'Padang',
-    image: padangImage,
-    description: 'Padang, ibu kota Sumatera Barat, menawarkan pesona pantai yang memukau serta kekayaan kuliner yang tak tertandingi.',
-    categories: ['Pantai', 'Kuliner', 'Sejarah'],
-  },
-  {
-    name: 'Bukittinggi',
-    image: bukittinggiImage,
-    description: 'Bukittinggi terkenal dengan Jam Gadang, ikon sejarah yang dikelilingi oleh keindahan alam dan kebudayaan Minangkabau.',
-    categories: ['Sejarah', 'Alam', 'Budaya'],
-  },
-];
-
-const relatedDestinations = [
-  {
-    name: 'Danau Singkarak',
-    location: 'Solok',
-    image: relatedImage,
-    rating: 4.7,
-    reviews: 340,
-  },
-  // Duplicate the object for demonstration purposes
-  {
-    name: 'Danau Singkarak',
-    location: 'Solok',
-    image: relatedImage,
-    rating: 4.7,
-    reviews: 340,
-  },
-  {
-    name: 'Danau Singkarak',
-    location: 'Solok',
-    image: relatedImage,
-    rating: 4.7,
-    reviews: 340,
-  },
-  {
-    name: 'Danau Singkarak',
-    location: 'Solok',
-    image: relatedImage,
-    rating: 4.7,
-    reviews: 340,
-  },
-];
+import data from '../data/data.json';
 
 const LocationSection = () => {
-  const [activeLocation, setActiveLocation] = useState(locations[0]);
+  const [activeLocation, setActiveLocation] = useState(data.locations[0]);
+  const [locations, setLocations] = useState([]);
+  const [relatedDestinations, setRelatedDestinations] = useState([]);
+
+  useEffect(() => {
+    setLocations(data.locations);
+    setRelatedDestinations(data.relatedDestinations);
+  }, []);
 
   return (
-    <section className="py-16">
-      <div className="text-center mb-8">
-        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold font-poppins mb-4">Jelajahi Destinasi Wisata Menarik</h2>
-        <p className="text-lg md:text-xl font-light font-poppins">Rencanakan perjalanan wisata anda dengan panduan, kiat perjalanan, informasi destinasi, dan inspirasi dari kami.</p>
+    <section className="py-16 w-full md:w-10/12 flex flex-col justify-center items-center mx-auto">
+      <div className="text-center mb-8 max-w-2xl">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-medium font-poppins mb-4">Jelajahi Destinasi Wisata Menarik</h2>
+        <p className="text-base md:text-lg font-light font-poppins">Rencanakan perjalanan wisata anda dengan panduan, kiat perjalanan, informasi destinasi, dan inspirasi dari kami.</p>
       </div>
 
       <div className="flex justify-center mb-8">
         {locations.map((location, index) => (
-          <button key={index} className={`px-6 py-2 mx-2 rounded-full font-poppins font-medium ${activeLocation.name === location.name ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`} onClick={() => setActiveLocation(location)}>
+          <button
+            key={index}
+            className={`px-6 py-2 mx-2 rounded-full font-poppins font-medium ${activeLocation.name === location.name ? 'bg-green-700 text-white' : 'bg-gray-200 text-gray-700'} transition duration-300 transform hover:scale-105`}
+            onClick={() => setActiveLocation(location)}
+          >
             {location.name}
           </button>
         ))}
       </div>
 
-      <div className="relative w-full max-w-4xl mx-auto mb-12">
-        <img src={activeLocation.image} alt={activeLocation.name} className="w-full rounded-lg shadow-lg" />
+      <div className="relative w-full max-w-screen-lg mx-auto mb-20">
+        <img src={activeLocation.image} alt={activeLocation.name} className="w-full shadow-lg h-96 object-cover" />
 
-        <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 p-6 rounded-b-lg text-white">
+        <div className="absolute -bottom-16 left-0 right-0 mx-auto bg-slate-500 bg-opacity-80 p-6 text-white w-11/12">
           <h3 className="text-2xl font-bold font-poppins mb-2">{activeLocation.name}</h3>
           <p className="mb-4">{activeLocation.description}</p>
           <div className="flex flex-wrap">
@@ -98,10 +50,10 @@ const LocationSection = () => {
       <div className="max-w-6xl mx-auto">
         <div className="flex justify-between items-center mb-4">
           <h3 className="text-2xl font-bold font-poppins">Destinasi Terkait</h3>
-          <button className="text-blue-500 font-poppins">Lihat Semua</button>
+          <button className="text-blue-500 font-poppins transition duration-300 transform hover:scale-105">Lihat Semua</button>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {relatedDestinations.map((destination, index) => (
             <DestinationCard key={index} {...destination} />
           ))}
